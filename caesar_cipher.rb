7018b093de4f1f7ed60df28c75317aff3210bac6
rodrigosites <rodrigo.sites@gmail.com>
2023-04-08 20:58:20 -0300

    Adjusted caesar_cipher.rb by rubocop indications

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,14 +3,14 @@ def caesar_cipher(string, shift_factor)
   upcase_letters = (65..90).to_a
   string_bytes = string.bytes
   string_bytes.each_index do |index|
-    if downcase_letters.include? (string_bytes[index])
-      if downcase_letters.include? (string_bytes[index] - shift_factor)
+    if downcase_letters.include?(string_bytes[index])
+      if downcase_letters.include?(string_bytes[index] - shift_factor)
         string_bytes[index] -= shift_factor
       else
         string_bytes[index] = 123 - (97 - (string_bytes[index] - shift_factor))
       end
-    elsif upcase_letters.include? (string_bytes[index])
-      if upcase_letters.include? (string_bytes[index] - shift_factor)
+    elsif upcase_letters.include?(string_bytes[index])
+      if upcase_letters.include?(string_bytes[index] - shift_factor)
         string_bytes[index] -= shift_factor
       else
         string_bytes[index] = 91 - (65 - (string_bytes[index] - shift_factor))
@@ -20,5 +20,4 @@ def caesar_cipher(string, shift_factor)
   end
 end
 
-caesar_cipher("Indeed, this text looks very complex after cryptography!",5)
-
+caesar_cipher('Indeed, this text looks very complex after cryptography!', 5)
