295c06a4a1228c3715dcefc394a4ad2986eaa76e
rodrigosites <rodrigo.sites@gmail.com>
2023-09-13 23:37:45 -0300

    event manager phone number assignment

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,35 +1,61 @@
 require 'csv'
+require 'erb'
 require 'google/apis/civicinfo_v2'
-
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts 'Event Manager Initialized!'
+def clean_phone_number(phone_number)
+  phone_number_length = phone_number.to_s.length
+  if phone_number_length == 11 && phone_number.to_s[0] == "1"
+    phone_number.to_s[1..10]
+  elsif phone_number_length == 10
+    phone_number
+  else
+    'bad number'
+  end
+end
 
-contents = CSV.open(
-  'event_attendees.csv', 
-  headers: true,
-  header_converters: :symbol
-  ) if File.exists?('./event_attendees.csv')
-
-  contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-  rescue => exception
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-  end  
+  end
+end
 
-  puts "#{name} #{zipcode} #{legislators}"
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'Event Manager Initialized!'
+
+if File.exist?('./event_attendees.csv')
+  contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+end
+
+template_letter = File.read('./form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 end
